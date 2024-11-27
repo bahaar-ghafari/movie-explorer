e0ed51a91baf4680ce754b9d83d9136597e2bbb5
bahar <bahaarghafari@gmail.com>
2024-11-27 15:39:38 +0100

    feat: add mivilist component

---
+++ src/components/layout/MovieList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div v-if="movies.length === 0" class="text-gray-500 mt-4">No movies found.</div>
+    <ul v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+      <li
+        v-for="movie in movies"
+        :key="movie.imdbID"
+        class="border rounded-lg shadow-md overflow-hidden"
+      >
+        <img
+          :src="movie.Poster !== 'N/A' ? movie.Poster : placeholderImage"
+          :alt="movie.Title"
+          class="w-full h-56 object-cover"
+        />
+        <div class="p-2 bg-white">
+          <h3 class="font-semibold text-lg truncate text-gray-600">{{ movie.Title }}</h3>
+          <p class="text-sm text-gray-600">{{ movie.Year }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue'
+import type { Movie } from '@/services/api/types/movies'
+
+export default defineComponent({
+  name: 'MovieList',
+  props: {
+    movies: {
+      type: Array as PropType<Movie[]>,
+      required: true,
+    },
+  },
+  data() {
+    // TODO:add image
+    return {
+      placeholderImage: 'PLACEHOLDER_IMAGE',
+    }
+  },
+})
+</script>
+
+<style scoped></style>
