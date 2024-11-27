56f33962e681e8c464986593ee583dc0f9bcfcb3
bahar <bahaarghafari@gmail.com>
2024-11-27 20:08:18 +0100

    feat: add favorite sore of pinia

---
+++ src/stores/favoritesStore.ts
@@ -0,0 +1,23 @@
+import type { Movie } from '@/types/movies'
+import { defineStore } from 'pinia'
+
+export const useFavoritesStore = defineStore('favorites', {
+  state: () => ({
+    favorites: [] as Movie[],
+  }),
+  getters: {
+    isFavorite: (state) => (imdbID: string) => {
+      return state.favorites.some((movie) => movie.imdbID === imdbID)
+    },
+  },
+  actions: {
+    addFavorite(movie: Movie) {
+      if (!this.isFavorite(movie.imdbID)) {
+        this.favorites.push(movie)
+      }
+    },
+    removeFavorite(imdbID: string) {
+      this.favorites = this.favorites.filter((movie) => movie.imdbID !== imdbID)
+    },
+  },
+})
