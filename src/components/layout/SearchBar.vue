6837f791c9f95b0ed0420d466ca4f20d2e2c8ffe
bahar <bahaarghafari@gmail.com>
2024-11-28 19:23:53 +0100

    feat: add query limitation in search

--- src/components/layout/SearchBar.vue
+++ src/components/layout/SearchBar.vue
@@ -15,6 +15,7 @@ import { defineComponent, ref } from 'vue'
 import InputField from '@/components/base/CustomInput.vue'
 import CustomButton from '@/components/base/CustomButton.vue'
 import SearchIcon from '@/assets/svg/SearchIcon.vue'
+import { useToast } from 'vue-toastification'
 
 export default defineComponent({
   name: 'SearchBar',
@@ -22,10 +23,13 @@ export default defineComponent({
   emits: ['search'],
   setup(_, { emit }) {
     const searchQuery = ref('')
+    const toast = useToast()
 
     const handleSearch = () => {
-      if (searchQuery.value.trim()) {
+      if (searchQuery.value.trim().length > 3) {
         emit('search', searchQuery.value)
+      } else {
+        toast.info('Please enter at least 3 characters.')
       }
     }
 
