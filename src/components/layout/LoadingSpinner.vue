9403a9c4aae26ca1232e3f37aa10ae821756908c
bahar <bahaarghafari@gmail.com>
2025-01-04 19:43:04 +0100

    style: fix loading style

--- src/components/layout/LoadingSpinner.vue
+++ src/components/layout/LoadingSpinner.vue
@@ -2,15 +2,18 @@
   <div class="flex justify-center items-center" role="status" aria-label="Loading">
     <div
       :class="[
-        'animate-spin rounded-full border-t-4 border-solid',
-        `h-${size} w-${size} border-t-${thickness} border-${color}`,
+        'animate-spin rounded-full border-solid',
+        `h-${Number(size)}`,
+        `w-${Number(size)}`,
+        `border-t-${Number(thickness)}`,
+        `border-${color}`,
       ]"
     ></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, type PropType } from 'vue'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'LoadingSpinner',
